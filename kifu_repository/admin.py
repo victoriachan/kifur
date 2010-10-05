de1f1dcb907a706b91798fbee7bc0918bb498351
Victoria Chan <contact@victoriac.net>
2010-10-05 13:12:12 -0500

    Tweaked models (use auto many-to-many intermediaries) and add objects to Admin

--- kifu_repository/admin.py
+++ kifu_repository/admin.py
@@ -1,4 +1,15 @@
 from kifur.kifu_repository.models import Kifu
+from kifur.kifu_repository.models import Player
+from kifur.kifu_repository.models import Tag
 from django.contrib import admin
 
-admin.site.register(Kifu)
+class KifuAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('SGF metadata', {'fields': ['sgf_text', 'player_white', 'player_black', 'board_size', 'handicap', 'komi', 'rules', 'event', 'date_recorded']}),
+        ('Kifur metadata', {'fields': ['description', 'tags', 'visibility', 'date_published']}),
+    ]
+    list_display = ('player_white', 'player_black', 'board_size', 'date_recorded', 'handicap')
+    
+admin.site.register(Kifu, KifuAdmin)
+admin.site.register(Player)
+admin.site.register(Tag)
--- kifu_repository/models.py
+++ kifu_repository/models.py
@@ -10,16 +10,22 @@ class Player(models.Model):
     full_name = models.CharField(max_length=150, unique=True)
     rank = models.CharField(max_length=30)
     description = models.CharField(max_length=200)
-    keywords = models.ManyToManyField(Tag, through='Player_Tags')
+    tags = models.ManyToManyField(Tag, related_name='players')
     
     def __unicode__(self):
         return self.full_name
 
 class Kifu(models.Model):
     VISIBILITY_CHOICES = (
-        (u'private', u'Only you can see this'),
-        (u'everyone', u'Everyone can see this'),
+        (u'private', u'Private'),
+        (u'everyone', u'Public'),
         # (u'friends', u'Only your friends can see this'),
+    )
+    RULES_CHOICES = (
+        (u'AGA', u'American Go Association rules'),
+        (u'GOE', u'Ing rules of Goe'),
+        (u'Japanese', u'Nihon-Kiin rules (Japanese)'),
+        (u'NZ', u'New Zealand rules'),
     )
     sgf_text = models.TextField()
     player_white = models.ForeignKey(Player, related_name='player_white')
@@ -27,11 +33,11 @@ class Kifu(models.Model):
     board_size = models.PositiveIntegerField(default=19)
     handicap = models.PositiveIntegerField(default=0)
     komi = models.DecimalField(max_digits=5, decimal_places=1, default=6.5)
-    rules = models.CharField(max_length=30, default='Japanese')
+    rules = models.CharField(max_length=30, default='Japanese', choices=RULES_CHOICES)
     result = models.CharField(max_length=20)
     event = models.CharField(max_length=200, null=True)
     description = models.CharField(max_length=200, null=True)
-    keywords = models.ManyToManyField(Tag, through='Kifu_Tags')
+    tags = models.ManyToManyField(Tag, related_name='kifus')
     visibility = models.CharField(max_length=10, choices=VISIBILITY_CHOICES, default='private')
     date_recorded = models.DateField('date recorded')
     date_published = models.DateTimeField('date published')
@@ -39,11 +45,3 @@ class Kifu(models.Model):
     
     def __unicode__(self):
         return '%s vs %s : %s' % (self.player_white.full_name, self.player_black.full_name, self.result)
-    
-class Kifu_Tags(models.Model):
-    tag = models.ForeignKey(Tag)
-    kifu = models.ForeignKey(Kifu)
-    
-class Player_Tags(models.Model):
-    tag = models.ForeignKey(Tag)
-    player = models.ForeignKey(Player)
