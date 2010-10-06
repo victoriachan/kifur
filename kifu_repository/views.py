c40a407bc94cb1c40986a992f741a7b72a4a3b27
Victoria Chan <contact@victoriac.net>
2010-10-06 11:28:16 -0500

    Just changing var names. Really procrastinating here huh?

--- kifu_repository/views.py
+++ kifu_repository/views.py
@@ -15,13 +15,13 @@ def kifu_add(request):
     return HttpResponse("kifu_add")
     
 def kifu_edit(request, kifu_id):
-    this_kifu = get_object_or_404(Kifu, pk=kifu_id)
-    return render_to_response('kifu_repository/kifu_edit.html', {'kifu': this_kifu})
+    k = get_object_or_404(Kifu, pk=kifu_id)
+    return render_to_response('kifu_repository/kifu_edit.html', {'kifu': k})
 
 def kifu_detail(request, kifu_id):
-    this_kifu = get_object_or_404(Kifu, pk=kifu_id)
-    return render_to_response('kifu_repository/kifu_detail.html', {'kifu': this_kifu})
+    k = get_object_or_404(Kifu, pk=kifu_id)
+    return render_to_response('kifu_repository/kifu_detail.html', {'kifu': k})
 
 def player_detail(request, player_id):
-    this_player = get_object_or_404(Player, pk=player_id)
-    return render_to_response('kifu_repository/player_detail.html', {'player': this_player})
+    p = get_object_or_404(Player, pk=player_id)
+    return render_to_response('kifu_repository/player_detail.html', {'player': p})
