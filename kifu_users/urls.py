9b66e877656d976143502eea5a3ad1887ed05ae3
Victoria Chan <contact@victoriac.net>
2010-11-21 17:31:22 +0000

    Also show profile via user id

--- kifu_users/urls.py
+++ kifu_users/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('kifur.kifu_users.views',
+    (r'^user/(?P<user_id>\d+)/$', 'user_profile_from_id'),
     (r'^user/(?P<slug>[a-zA-Z0-9_.-]+)/$', 'user_profile'),
 )
--- kifu_users/views.py
+++ kifu_users/views.py
@@ -5,3 +5,7 @@ from kifur.kifu_users.models import UserProfile
 def user_profile(request, slug):
     u = get_object_or_404(UserProfile, slug=slug)
     return render_to_response('kifu_users/user_profile.html', {'user': u})
+
+def user_profile_from_id(request, user_id):
+    u = get_object_or_404(User, pk=user_id)
+    return render_to_response('kifu_users/user_profile.html', {'user': u})    
